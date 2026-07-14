30d56e03d08cd1d72cd3bfd2874d87b954d4a65b
Bari <barahonajose279@gmail.com>
2026-07-14 10:17:45 -0500

    Enhance navigation animations in RootLayout component

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -5,10 +5,10 @@ export default function RootLayout() {
   return (
     <>
       <StatusBar style="light" hidden />
-      <Stack screenOptions={{ headerShown: false }}>
+      <Stack screenOptions={{ headerShown: false, animation: "slide_from_right" }}>
         <Stack.Screen name="index" />
         <Stack.Screen name="game" />
-        <Stack.Screen name="history" />
+        <Stack.Screen name="history" options={{ animation: "slide_from_bottom" }} />
       </Stack>
     </>
   );
